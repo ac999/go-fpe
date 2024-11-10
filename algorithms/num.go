09c6f6aa04242be76273e9abddbd9c4d8fda88d9
Andrei Cristian <andreicristian6@protonmail.com>
2024-11-10 12:44:47 +0200

    Correct NumBits function

--- algorithms/num.go
+++ algorithms/num.go
@@ -29,16 +29,17 @@ func NumRadix(X string, radix int) (uint64, error) {
 	return x, nil
 }
 
-// NumBits(X) function implements Algorithm 2: NUM(X) from NIST Special Publication 800-38G.
-// Converts a byte string represented in bits to an integer.
+// NumBits converts a binary string X, represented in bits, to an integer.
 func NumBits(X string) (uint64, error) {
 	var x uint64 = 0
 	for i := 0; i < len(X); i++ {
-		// if X[i] != '0' && X[i] != '1' {
-		// 	return 0, fmt.Errorf("invalid character '%c' in binary string", X[i])
-		// }
+		// Check for valid binary characters.
+		if X[i] != '0' && X[i] != '1' {
+			return 0, fmt.Errorf("invalid character '%c' in binary string", X[i])
+		}
 
-		bit := X[i] // - '0' // Convert character '0' or '1' to integer 0 or 1
+		// Convert '0' or '1' to integer 0 or 1 by subtracting '0'.
+		bit := X[i] - '0'
 		x = 2*x + uint64(bit)
 	}
 
